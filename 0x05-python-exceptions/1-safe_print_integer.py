d2e05d8935fdd72d0c5a254a0803cf05b2d07eeb
KawinoTech <techkawino@gmail.com>
2024-10-23 19:20:27 +0300

    fixed

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,7 @@
 def safe_print_integer(value):
     try:
         i = 1 + value
-        print(value)
+        print("{:d}".format(value))
         return True
     except Exception as e:
         return False
