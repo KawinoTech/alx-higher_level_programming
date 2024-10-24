86060d919496248820581651ceda56d3e4767337
KawinoTech <techkawino@gmail.com>
2024-10-24 16:55:32 +0300

    Classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Defines blueprint for square objects"""
+    def __init__(self, size):
+        """Args:
+            size (str): size of square."""
+        self.__size = size
