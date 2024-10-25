8d361545d0341d89d82edf88b49d9ec292beb361
KawinoTech <techkawino@gmail.com>
2024-10-25 11:22:25 +0300

    Readme

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -71,6 +71,7 @@ class Rectangle:
             return "{:s}\n".format(Rectangle.print_symbol * self.__width) * self.__height
     def __del__(self):
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_1, Rectangle):
@@ -79,7 +80,7 @@ class Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1 == rect_2:
             return rect_1
-        return max(rect_1, rect_2)
+        return max(rect_1.area(), rect_2.area())
     @classmethod
     def square(cls, size=0):
         return cls(size, size)
