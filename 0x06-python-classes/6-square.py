bba98c29a754a8cc6eaaf5cf3d54eb2b560452a7
KawinoTech <techkawino@gmail.com>
2024-10-28 13:14:26 +0300

    Exceptions

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,53 +1,59 @@
 #!/usr/bin/python3
-"""Defines a class Square"""
 
 
 class Square:
-    """Defines blueprint for square objects"""
+    """
+    class square that has attributes:
+        size
+    some attributes are protected from input.
+    """
     def __init__(self, size=0, position=(0, 0)):
         """
-        Args:
-            size (str): size of square.
-        Raises:
-                TypeError: if size is not an integer
-                ValueError: if size is less than 0
-                TypeError: if any element of tuple is less than 0
+        initialization function for our square clasee
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        else:
+        if self.__validate_size(size):
             self.__size = size
+        if self.__validate_pos(position):
             self.__position = position
-
-    def area(self):
-        """Returns the area of the square object"""
-        return self.__size**2
 
     @property
     def size(self):
         """
-        int: instance property size getter
-        @size.setter: instance property size getter
-                    Raises:
-                        TypeError: if size is not an integer
-                        ValueError: if size is less than 0
+        getter for size attribute
         """
         return self.__size
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        else:
+        """
+        setter for size attribute
+        """
+        if self.__validate_size(value):
             self.__size = value
 
-    def my_print(self):
-        """prints in stdout the square with the character #
+    @property
+    def position(self):
+        """
+        getter for position attribute
+        """
+        return self.__position
 
+    @position.setter
+    def position(self, value):
+        """
+        setter for position attribute
+        """
+        if self.__validate_pos(value):
+            self.__position = value
+
+    def area(self):
+        """
+        calculates the area of the square
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """
         prints the square using '#' characters
         also takes into account position (x, y) offsets
         """
@@ -66,34 +72,24 @@ class Square:
             for j in range(0, self.__size):
                 print("#", end='')
             print()
-    @property
-    def position(self):
-        """
-        tuple: instance property position getter
-        @position.setter: instance property size getter
-                    Raises:
-                        TypeError: position must be a tuple of 2 positive integers
-        """
-        return self.__position
 
-    @position.setter
-    def position(self, value):
-        if value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
+    def __validate_size(self, size):
+        """
+        validates the size, checking for errors
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         else:
-            self.__position = value
+            return True
+        return False
 
-my_square_1 = Square(3)
-my_square_1.my_print()
-
-print("--")
-
-my_square_2 = Square(3, (1, 1))
-my_square_2.my_print()
-
-print("--")
-
-my_square_3 = Square(3, (3, 0))
-my_square_3.my_print()
-
-print("--")
+    def __validate_pos(self, position):
+        """
+        validates the position, checking for type errors
+        """
+        if not isinstance(position, type((0, 0))):
+            raise TypeError("position must be a tuple of 2 positive integers")
+            return False
+        return True
