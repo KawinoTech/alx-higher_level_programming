6bbe0349e1e90174d854236780263f5ebe08e47b
KawinoTech <techkawino@gmail.com>
2024-11-01 11:11:20 +0300

    fixed

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+This is the "Student"  module.
+
+This module provides a Student class.
+"""
+
+
+class Student:
+    """A Student class with attributes first_name, last_name and age,
+    methods to_json
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance
+        """
+        dict_1 = {}
+
+        if attrs:
+            for i in range(len(attrs)):
+                try:
+                    dict_1[attrs[i]] = getattr(self, attrs[i])
+                except AttributeError:
+                    continue
+            return dict_1
+        else:
+            return vars(self)
+
+    def reload_from_json(self, json):
+        """
+        Function that replaces all attributes of the Student instance
+
+        Args:
+            json: dictionary containing the target state of instance
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
