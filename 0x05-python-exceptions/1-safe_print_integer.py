49fdebdac1d2b0db7b7c644b4e3e8b2b92926611
KawinoTech <techkawino@gmail.com>
2024-11-01 11:21:02 +0300

    Add files

--- 0x05-python-exceptions/1-safe_print_integer.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-def safe_print_integer(value):
-    try:
-        i = 1 + value
-        print("{:d}".format(value))
-        return True
-    except Exception as e:
-        return False
---
+++ 0x09-python-everything_is_object/main.py
@@ -0,0 +1,4 @@
+with open("0-answer.txt", 'r', encoding='ASCII') as f:
+    for line in f:
+        print(line)
+    print(f.encoding)
