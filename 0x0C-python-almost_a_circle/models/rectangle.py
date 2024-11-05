6e1d4777002fcb246be0e34299430f63d5e17d5c
Kawino1998 <jeremykawino@gmail.com>
2024-11-05 08:33:56 +0300

    Almost a circe

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Module containing Rectangle class. Sub-class of Base
+...
 """
 
 
@@ -9,24 +9,12 @@ from base import Base
 
 class Rectangle(Base):
     """
-    Class Rectangle. subclass of Base
-
-    Attrs:
-        private instance width: width of rectangle
-        private instance height: width of height
-        private instance x: x
-        private instance y: y
+    ...
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """
-        Initialize instance attributes.
-        Args:
-            width (int): Width of the rectangle.
-            height (int): Height of the rectangle.
-            x (int): X position of the rectangle.
-            y (int): Y position of the rectangle.
-            id (int): ID of the instance.
+        ...
         """
         super().__init__(id)
         try:
@@ -45,18 +33,14 @@ class Rectangle(Base):
     @property
     def width(self):
         """
-        Get width.
-        Returns:
-            width
+        ...
         """
         return self.__width
 
     @width.setter
     def width(self, value):
         """
-        Set width
-        Args:
-            value (int): value
+        ...
         """
         try:
             self.validator('width', value)
@@ -68,18 +52,14 @@ class Rectangle(Base):
     @property
     def height(self):
         """
-        Get height
-        Returns:
-            height
+        ...
         """
         return self.__height
 
     @height.setter
     def height(self, value):
         """
-        Set height
-        Args:
-            value (int): value
+        ...
         """
         try:
             self.validator('height', value)
@@ -91,18 +71,14 @@ class Rectangle(Base):
     @property
     def x(self):
         """
-        Get x
-        Returns:
-            x
+        ...
         """
         return self.__x
 
     @x.setter
     def x(self, value):
         """
-        Set x
-        Args:
-            value (int): value
+        ...
         """
         try:
             self.validator('x', value)
@@ -114,18 +90,14 @@ class Rectangle(Base):
     @property
     def y(self):
         """
-        Get y
-        Returns:
-            y
+        ...
         """
         return self.__y
 
     @y.setter
     def y(self, value):
         """
-        Set y
-        Args:
-            value (int): value
+        ...
         """
         try:
             self.validator('y', value)
@@ -137,14 +109,7 @@ class Rectangle(Base):
     @staticmethod
     def validator(name, value):
         """
-        Validates user input
-        Args:
-            name (str): string
-            value (int): value to be validated
-        Raises:
-            TypeError: If `value` is not an integer.
-            ValueError: If `x` or `y` is less than 0.
-            ValueError: If `width` or `height` is less than or equal to 0.
+        ...
         """
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
@@ -158,16 +123,13 @@ class Rectangle(Base):
 
     def area(self):
         """
-        Find area of rectangle
-        Returns:
-            area of rectangle
+        ...
         """
         return self.__width * self.__height
 
     def display(self):
         """
-        Displays a representation of a rectangle
-        using '#'
+        ...
         """
         for i in range(self.__height):
             print(self.__width * '#', end="")
@@ -175,8 +137,7 @@ class Rectangle(Base):
 
     def __str__(self):
         """
-        Returns the object representation of
-        an instance
+        ...
         """
 
         return "[{:s}] ({:d}) {:d}/{:d} - {:d}/{:d}".format(
@@ -185,10 +146,7 @@ class Rectangle(Base):
 
     def update(self, *args):
         """
-        Updates instance attributes.
-
-        Args:
-            *args (int): varying number of arguments
+        ...
         """
         attrs = ['id', 'width', 'height', 'x', 'y']
         for i in range(len(args)):
