9606174d05e83f7e8747a9420d6aaa1c8e9238af
KawinoTech <techkawino@gmail.com>
2024-11-08 14:05:13 +0300

    SQL

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 -- displays the number of records with 
 -- id = 89 in the table first_table
-SELECT COUNT(user_id) AS "" FROM people WHERE id = 89;
+SELECT COUNT(id) AS "" FROM people WHERE id = 89;
