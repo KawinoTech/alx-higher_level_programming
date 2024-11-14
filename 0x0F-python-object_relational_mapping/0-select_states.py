2c93ce4216438774e87f639a2026f83ee3407a71
Kawino1998 <jeremykawino@gmail.com>
2024-11-14 17:21:54 +0300

    ORM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,52 @@
 #!/usr/bin/python3
-"""lists all states from the database hbtn_0e_0_usa"""
+"""
+This script connects to a MySQL database using command-line arguments for
+username, password, and database name.
+It retrieves all records from the 'states'
+table and displays them in ascending order by their 'id'.
 
-if __name__ == '__main__':
+Usage:
+    python script_name.py <username> <password> <database>
 
+Positional arguments:
+    username    The MySQL username.
+    password    The MySQL password.
+    database    The name of the MySQL database to connect to.
+
+Example:
+    python script_name.py root mypassword mydatabase
+
+Dependencies:
+    - MySQLdb (You can install it via: pip install mysqlclient)
+
+Notes:
+    - This script is designed to be run directly, not as an imported module.
+    - If executed directly (i.e., when __name__ == "__main__"),
+    the script will do nothing.
+"""
+
+if __name__ == "__main__":
+    from sys import argv
     import MySQLdb
-    import sys
 
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    # Establishing a connection to the MySQL database
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        port=3306
+    )
 
+    # Creating a cursor object to interact with the database
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+
+    # Executing an SQL query to retrieve all rows from the 'states' table
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+
+    # Fetching all rows from the result of the query
     rows = cur.fetchall()
     for row in rows:
         print(row)
+    cur.close()
+    db.close()
