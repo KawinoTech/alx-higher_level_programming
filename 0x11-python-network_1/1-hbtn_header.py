9409591a8719ad0d4da35c1b1494e2f27d99edec
KawinoTech <techkawino@gmail.com>
2024-12-20 12:40:49 +0300

    Task Done

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,15 +6,6 @@ Description:
 This module fetches and displays the value of the
 `X-Request-Id` header from the HTTP response
 of a specified URL. It uses `urllib.request.urlopen` to send the request.
-
-Usage:
-- Run the script from the command line, passing the URL as the first argument:
-  $ ./script_name.py <URL>
-- Outputs the value of the `X-Request-Id` header.
-
-Dependencies:
-- sys: Used to read the URL argument from the command line.
-- urllib.request: Used to make the HTTP request.
 """
 
 import urllib.request
--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,28 +3,9 @@
 Module: Fetch URL Content and Handle Errors
 
 Description:
-This module fetches the content from a specified URL, decodes the response body into UTF-8, 
+This module fetches the content from a specified URL,
+decodes the response body into UTF-8, 
 and handles URL-related errors. It prints the error code if the URL is invalid.
-
-Usage:
-- Run the script, passing the URL as the first argument:
-  $ ./script_name.py <URL>
-- If the request is successful, the script decodes the content from the URL.
-- If an error occurs (e.g., invalid URL), it prints the error code.
-
-Dependencies:
-- sys: Used to fetch the URL from command-line arguments.
-- urllib: Used to make the HTTP request and handle errors.
-
-Example:
-$ ./script_name.py https://example.com
-Output:
-Decoded content from the page (if valid).
-
-Error Handling:
-If the URL is invalid, the script catches the error and prints an error message in the following format:
-Error code: <error_code>
-
 """
 if __name__ == "__main__":
     import urllib
